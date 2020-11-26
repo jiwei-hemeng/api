bdf034de5201d0c07a8b18b83a9c23e0caa01175
jiwei-hemeng <jiwei7065@sina.com>
2020-11-26 17:53:19 +0800

    项目初始化

---
+++ src/views/index/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="index">首页</div>
+</template>
+
+<script>
+  export default {
+    name: 'Index',
+    components: {},
+    data() {
+      return {}
+    },
+    created() {},
+    beforeDestroy() {},
+    mounted() {},
+    methods: {},
+  }
+</script>
+<style lang="scss" scoped></style>
